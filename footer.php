8db02f7e63971be1403298565fc39b44106a11bd
Thomas <drahcira.tw@gmail.com>
2015-02-25 15:21:01 -0800

    Adding all necessary folders and files/ create header and footer

---
+++ footer.php
@@ -0,0 +1,13 @@
+
+<div id="footer">
+	<div class="footer-social">
+		<a href="" target="_blank"><span class="icon-facebook2"></span></a>
+		<a href="" target="_blank"><span class="icon-instagram"></span></a>
+	</div>
+	<div class="bblogo"><img src="img/beachbody_challenge_logo.png" alt="BeachBody Logo"></div>
+	<div class="footer-contact-info">
+		<p>(999)999-999</p>
+		<p>michele.pipho.martin@gmail.com</p>
+	</div>
+	<p class="copyright">&copy; Michele Martin<?php echo date("Y") ?></p>
+</div>
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php include('header.php');?>
+
+<div class="hero">
+	<img src="" alt="" class="hero">
+</div>
+
+<div class="content">
+	
+</div>
+
+<?php include('footer.php');?>
+
+</div>
+<!-- End Container div -->
+</body>
+</html>
